d5a9488ca7bdbfbe6697d28cd5b03c88d17cbd55
Samboozle <samuel.a.bates@gmail.com>
2021-01-20 15:29:07 -0800

    stood up app; got basics of WASM code bundling working with unpkg

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgAid } from './helpers/unpkgAid';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
+
+const App = () => {
+  const [userCode, setUserCode] = useState<string>("");
+  const [spitCode, setSpitCode] = useState<string>("");
+  const WASM = useRef<any>();
+
+  const startWASM = async () => {
+    WASM.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  }
+
+  const handleUserInput = (e: ChangeEvent<HTMLInputElement>) => setUserCode(e.target.value);
+
+  const handleTranspile = async () => {
+    if (!WASM.current) return;
+
+    const result = await WASM.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgAid()],
+      define: {
+        "process.env.NODE_ENV": "'production'",
+        global: "window"
+      }
+    });
+
+    console.log(result)
+    setSpitCode(result.outputFiles[0].text);
+  }
+
+  useEffect(() => {
+    startWASM();
+  }, []);
+
+  return (
+    <div>
+      <textarea
+        onChange={ e => setUserCode(e.target.value) }
+        value={ userCode }
+      />
+      <br />
+      <button onClick={ handleTranspile }>Submit</button>
+      <br />
+      <pre>
+        { spitCode }
+      </pre>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <App />,
+  document.getElementById("root")
+)
